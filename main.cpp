2a06f70c0b3fd3905278fb78377453b65219796d
maenotakanori <115054954+maenotakanori@users.noreply.github.com>
2024-06-19 13:08:51 +0900

    a

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <Novice.h>
+#include<time.h>
 
 const char kWindowTitle[] = "LE2D_21_マエノ_タカノリ";
 
@@ -10,22 +11,40 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	int map[15][11] = {
 		{1,1,1,1,1,1,1,1,1,1,1},
-		{1,0,0,0,0,3,0,0,0,0,1},
+		{1,0,0,0,0,0,3,0,0,0,1},
 		{1,0,0,0,0,0,0,0,0,0,1},
 		{1,0,0,0,0,0,0,0,0,0,1},
 		{1,0,0,0,0,0,0,0,0,0,1},
 		{1,0,0,0,0,0,0,0,0,0,1},
-		{1,0,0,0,1,0,1,0,0,0,1},
 		{1,0,0,0,0,1,0,0,0,0,1},
-		{1,0,0,0,1,0,1,0,0,0,1},
+		{1,0,0,0,0,0,0,0,0,0,1},
 		{1,0,0,0,0,0,0,0,0,0,1},
 		{1,0,0,0,0,0,0,0,0,0,1},
 		{1,0,0,0,0,0,0,0,0,0,1},
 		{1,0,0,0,0,0,0,0,0,0,1},
 		{1,0,0,0,0,2,0,0,0,0,1},
+		{1,0,0,0,0,0,0,0,0,0,1},
 		{1,1,1,1,1,1,1,1,1,1,1} };
 
-	//0.空白　1.ブロック　2.プレイヤー　3.対象地点 
+	//0.空白　1.ブロック　2.プレイヤー　3.目的地 
+
+	int mapNumber[15][11] = {
+		{0,0,0,0,0,0,0,0,0,0,0},
+		{0,0,0,0,0,0,0,0,0,0,0},
+		{0,0,0,0,0,0,0,0,0,0,0},
+		{0,0,0,0,0,0,0,0,0,0,0},
+		{0,0,0,0,0,0,0,0,0,0,0},
+		{0,0,0,0,0,0,0,0,0,0,0},
+		{0,0,0,0,0,0,0,0,0,0,0},
+		{0,0,0,0,0,0,0,0,0,0,0},
+		{0,0,0,0,0,0,0,0,0,0,0},
+		{0,0,0,0,0,0,0,0,0,0,0},
+		{0,0,0,0,0,0,0,0,0,0,0},
+		{0,0,0,0,0,0,0,0,0,0,0},
+		{0,0,0,0,0,0,0,0,0,0,0},
+		{0,0,0,0,0,0,0,0,0,0,0},
+		{0,0,0,0,0,0,0,0,0,0,0} };
+	int mapDebug = 0;
 
 	int blocksize = 32;
 	int blockposx = 480;
@@ -41,10 +60,13 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	int moovAI = 0;
 
-	int playerposX = 0;
-	int playerposY = 0;
-	int goalposX = 0;
-	int goalposy = 0;
+	int playerPosX = 0;
+	int playerPosY = 0;
+	int goalPosX = 0;
+	int goalPosY = 0;
+	srand((unsigned int)time(nullptr));
+	int randPosX = rand() % 9;
+	int randPosY = rand() % 13;
 
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
@@ -62,13 +84,24 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓更新処理ここから
 		///
-
 		//移動
 		if (keys[DIK_W]) { moovW = 1; timerON = 1; }
 		if (keys[DIK_A]) { moovA = 1; timerON = 1; }
 		if (keys[DIK_S]) { moovS = 1; timerON = 1; }
 		if (keys[DIK_D]) { moovD = 1; timerON = 1; }
 		if (keys[DIK_SPACE]) { moovAI = 1; }
+		if(keys[DIK_Q]){ mapDebug = 1; }
+		else { mapDebug = 0; }
+		if (keys[DIK_E] && timerON == 0	) {
+			timerON = 1;
+			
+			randPosX = rand() % 9;
+			randPosY = rand() % 13;
+			if (randPosX == 0) { randPosX = 9; }
+			if (randPosY == 0) { randPosY = 13; }
+
+			map[randPosY][randPosX] = 3;
+		}
 
 		if (timerON == 1) { timer--; }
 		if (timer <= 0) { timerON = 0; timer = 10; }
@@ -102,6 +135,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 					if (map[i][j] == 2 && map[i + 1][j] != 1) {
 						map[i + 1][j] = 2; map[i][j] = 0;
 						moovS = 0;
+						i = 99;
 						break;
 					}
 				}
@@ -122,16 +156,46 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		//ポジション
 		for (int i = 0; i < 15; i++) {
 			for (int j = 0; j < 11; j++) {
-				if (map[i][j] == 2) { playerposX = j; playerposY = i; }
-				if (map[i][j] == 3) { goalposX = j; goalposy = i; }
+				if (map[i][j] == 2) { playerPosX = j; playerPosY = i; }
+				if (map[i][j] == 3) { goalPosX = j; goalPosY = i; }
 			}
 		}
 
-		//移動AI
-		if (moovAI == 1) {
-
+		//ルート調査
+		for (int i = 0; i < 15; i++) {
+			for (int j = 0; j < 11; j++) {
+				if (map[i][j] == 3) {
+					mapNumber[i][j] = 1;
+				}
+				for (int mapCount = 1; mapCount < 20; mapCount++) {
+					if (mapNumber[i][j] == mapCount) {
+						if (map[i - 1][j] == 0 && mapNumber[i - 1][j] == 0 || map[i - 1][j] == 2 && mapNumber[i - 1][j] == 0) { mapNumber[i - 1][j] = mapCount + 1; }
+						if (map[i][j - 1] == 0 && mapNumber[i][j - 1] == 0 || map[i][j - 1] == 2 && mapNumber[i][j - 1] == 0) { mapNumber[i][j - 1] = mapCount + 1; }
+						if (map[i + 1][j] == 0 && mapNumber[i + 1][j] == 0 || map[i + 1][j] == 2 && mapNumber[i + 1][j] == 0) { mapNumber[i + 1][j] = mapCount + 1; }
+						if (map[i][j + 1] == 0 && mapNumber[i][j + 1] == 0 || map[i][j + 1] == 2 && mapNumber[i][j + 1] == 0) { mapNumber[i][j + 1] = mapCount + 1; }
+					}
+				}
+			}
 		}
 
+
+
+		//移動AI
+		if (moovAI == 1) {
+			if (mapNumber[playerPosY][playerPosX] > mapNumber[playerPosY + 1][playerPosX]){ moovW = 1; timerON = 1; }
+			if (mapNumber[playerPosY][playerPosX] > mapNumber[playerPosY][playerPosX - 1]) { moovA = 1; timerON = 1; }
+			if (mapNumber[playerPosY][playerPosX] > mapNumber[playerPosY + 1][playerPosX]) { moovS = 1; timerON = 1; }
+			if (mapNumber[playerPosY][playerPosX] > mapNumber[playerPosY][playerPosX + 1]) { moovD = 1; timerON = 1; }
+		}
+
+		if (playerPosX == goalPosX && playerPosY == goalPosY) {
+			moovAI = 0;
+			for (int i = 0; i < 15; i++) {
+				for (int j = 0; j < 11; j++) {
+					mapNumber[i][j] = 0;
+				}
+			}
+		}
 
 		///
 		/// ↑更新処理ここまで
@@ -152,9 +216,27 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		}
 
 		//デバッグ
-		Novice::ScreenPrintf(0, 0, "timer %d", timer);
-		Novice::ScreenPrintf(0, 20, "posX %d", playerposX);
-		Novice::ScreenPrintf(0, 40, "posY %d", playerposY);
+		Novice::ScreenPrintf(0, 0, "posX %d", playerPosX);
+		Novice::ScreenPrintf(0, 20, "posY %d", playerPosY);
+		Novice::ScreenPrintf(0, 100, "moovW %d", moovW);
+		Novice::ScreenPrintf(0, 120, "moovA %d", moovA);
+		Novice::ScreenPrintf(0, 140, "moovS %d", moovS);
+		Novice::ScreenPrintf(0, 160, "moovD %d", moovD);
+
+		if (mapDebug == 1) {
+			for (int i = 0; i < 15; i++) {
+				for (int j = 0; j < 11; j++) {
+					for (int mapCount = 1; mapCount < 20; mapCount++) {
+						if (mapNumber[i][j] == mapCount) {
+							Novice::ScreenPrintf(j* blocksize + blockposx + 12, i* blocksize + blockposy + 8, "%d", mapCount);
+							Novice::DrawBox(j * blocksize + blockposx, i * blocksize + blockposy, blocksize, blocksize, 0.0f, BLUE, kFillModeWireFrame);
+						}
+						
+						
+					}
+				}
+			}
+		}
 
 		///
 		/// ↑描画処理ここまで
